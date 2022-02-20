3bf89e250e66f8d73cea48c6dd261ba640216622
Zeb Niazi <zebmustafa2010@gmail.com>
2022-02-20 19:53:41 +0500

    Apply rate limit , in which user can hit 15 request api in a minute only

--- src/main/java/com/marketing/processor/controller/MarketingDataController.java
+++ src/main/java/com/marketing/processor/controller/MarketingDataController.java
@@ -3,11 +3,16 @@ package com.marketing.processor.controller;
 import com.marketing.processor.domain.entity.MarketingData;
 import com.marketing.processor.service.MarketingDataService;
 import com.marketing.processor.service.UserService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +24,16 @@ public class MarketingDataController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/data")
+    private final Bucket bucket;
+
+    public MarketingDataController() {
+        Bandwidth limit = Bandwidth.classic(15, Refill.greedy(15, Duration.ofMinutes(1)));
+        this.bucket = Bucket4j.builder()
+                .addLimit(limit)
+                .build();
+    }
+
+    @GetMapping("/message-list")
     public ResponseEntity<List<MarketingData>> getAllData() {
         try {
             List<MarketingData> data = new ArrayList<>(marketingDataService.findAll());
@@ -29,17 +43,20 @@ public class MarketingDataController {
         }
     }
 
-    @PostMapping("/sendMessage")
+    @PostMapping("/send-message")
     public ResponseEntity<Void> createMessage(
             @RequestBody MarketingData marketingData) {
-        boolean result = marketingDataService.save(marketingData);
-        if (result) {
-            ResponseEntity
+        if (bucket.tryConsume(1)) {
+            boolean result = marketingDataService.save(marketingData);
+            if (!result) {
+                return ResponseEntity
+                        .status(HttpStatus.CONFLICT)
+                        .build();
+            }
+            return ResponseEntity
                     .status(HttpStatus.CREATED)
                     .build();
         }
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .build();
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
