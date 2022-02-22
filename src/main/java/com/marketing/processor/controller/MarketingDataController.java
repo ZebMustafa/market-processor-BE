0184ea9cb2dd5afc60ba670aa67388365bf3759c
Zeb Niazi <zebmustafa2010@gmail.com>
2022-02-22 05:38:45 +0500

    - Removing rate limit on request API - Open Socket and communicate with STOMP for live behaviour

--- src/main/java/com/marketing/processor/controller/MarketingDataController.java
+++ src/main/java/com/marketing/processor/controller/MarketingDataController.java
@@ -1,18 +1,15 @@
 package com.marketing.processor.controller;
 
 import com.marketing.processor.domain.entity.MarketingData;
+import com.marketing.processor.domain.enumeration.MessageBrokerEnum;
 import com.marketing.processor.service.MarketingDataService;
 import com.marketing.processor.service.UserService;
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Bucket4j;
-import io.github.bucket4j.Refill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +21,8 @@ public class MarketingDataController {
     @Autowired
     UserService userService;
 
-    private final Bucket bucket;
-
-    public MarketingDataController() {
-        Bandwidth limit = Bandwidth.classic(15, Refill.greedy(15, Duration.ofMinutes(1)));
-        this.bucket = Bucket4j.builder()
-                .addLimit(limit)
-                .build();
-    }
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @GetMapping("/message-list")
     public ResponseEntity<List<MarketingData>> getAllData() {
@@ -46,17 +37,17 @@ public class MarketingDataController {
     @PostMapping("/send-message")
     public ResponseEntity<Void> createMessage(
             @RequestBody MarketingData marketingData) {
-        if (bucket.tryConsume(1)) {
-            boolean result = marketingDataService.save(marketingData);
-            if (!result) {
-                return ResponseEntity
-                        .status(HttpStatus.CONFLICT)
-                        .build();
-            }
+        boolean result = marketingDataService.save(marketingData);
+        if (!result) {
             return ResponseEntity
-                    .status(HttpStatus.CREATED)
+                    .status(HttpStatus.CONFLICT)
                     .build();
         }
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        this.template.convertAndSend(
+                MessageBrokerEnum.BROKER_DESTINATION_TOPIC.getValue() + "/message",
+                marketingData);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
     }
 }
